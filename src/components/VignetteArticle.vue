0dc883da1c8851580051856ba4645dd6e162a714
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 16:27:30 +0200

    Ajout de l'intro au composant de la vignette

--- src/components/VignetteArticle.vue
+++ src/components/VignetteArticle.vue
@@ -3,6 +3,7 @@
         <img class="absolute " :src="image" alt="">
         <div class="w-[375px] h-[107px] absolute mt-[266px] rounded-t-18xl  bg-blanc-background"></div>
         <h1 class="absolute ml-5 mt-[150px] text-3xl font-asap text-left text-blanc-background">{{titre1}} <br> {{titre2}}</h1>
+        <h4 class="absolute ml-5 mt-[300px] font-poppins text-left text-vert-kaki-elements-importants">{{intro}}</h4>
     </div>
     
 </template>
@@ -13,6 +14,7 @@ export default {
     image: String,
     titre1: String,
     titre2: String,
+    intro: String, 
   }
 }
 </script>
