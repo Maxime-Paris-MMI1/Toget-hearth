22dad4cb8a9b789e0df2e2eaa151378b488081dd
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-17 15:27:04 +0200

    header logo final

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,8 +1,11 @@
 <template>
   <header class="bg-blanc-vert-title">
-    <div class="flex justify-between pb-10 shadow-xl">
+    <div class="flex justify-between pb-6 shadow-xl">
       <div class="pt-8 pl-6">
         <IconConnexion/>
+      </div>
+      <div class="pt-5">
+        <LogoSite/>
       </div>
       <div class="pt-12 pr-7">
         <MenuHamburger/>
@@ -14,8 +17,9 @@
 <script>
 import IconConnexion from "../components/icons/IconConnexion.vue" 
 import MenuHamburger from "../components/icons/MenuHamburger.vue" 
+import LogoSite from "../components/icons/LogoSite.vue" 
 export default {
   name: "Header",
-  components: {IconConnexion, MenuHamburger}
+  components: {IconConnexion, MenuHamburger, LogoSite}
 };
 </script>
