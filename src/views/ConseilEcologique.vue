32a905ea6a4555a28b2f962349ca97c9a0c96886
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-17 15:44:46 +0200

    Header page d'accueil finit

--- src/views/ConseilEcologique.vue
+++ src/views/ConseilEcologique.vue
@@ -1,15 +1,15 @@
 <template>
-  <header>
-    <div class="flex justify-between">
+  <header class="bg-blanc-vert-title">
+    <div class="flex justify-between pb-6 shadow-xl">
       <div class="pt-8 pl-6">
         <IconConnexion/>
+      </div>
+      <div class="pt-5">
+        <LogoSite/>
       </div>
       <div class="pt-12 pr-7">
         <MenuHamburger/>
       </div>
-    </div>
-    <div class="flex justify-center cursor-se-resize">
-      <logo-grand/>
     </div>
   </header>
 </template>
@@ -18,9 +18,8 @@
 import IconConnexion from "../components/icons/IconConnexion.vue" 
 import MenuHamburger from "../components/icons/MenuHamburger.vue" 
 import LogoSite from "../components/icons/LogoSite.vue" 
-import LogoGrand from "../components/icons/LogoGrand.vue" 
 export default {
   name: "Header",
-  components: {IconConnexion, MenuHamburger, LogoSite, LogoGrand}
+  components: {IconConnexion, MenuHamburger, LogoSite}
 };
 </script>
