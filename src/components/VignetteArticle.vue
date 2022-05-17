cd05103df1b6497d78313416e69be0858ae9b1dc
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 16:03:50 +0200

    Création du composants de la vignette article

---
+++ src/components/VignetteArticle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="relative">
+        <img class="absolute " :src="image" alt="">
+        <div class="w-[375px] h-[107px] absolute mt-[266px] rounded-t-18xl  bg-vert-fluo-bouton"></div>
+        <h1 class="absolute ml-5 mt-[150px] text-3xl font-asap text-left text-vert-moyen-deco">{{titre1}}</h1>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    titre1: String,
+  }
+}
+</script>
---
+++ src/views/Conseil1View.vue
@@ -0,0 +1,11 @@
+<template>
+    <VignetteArticle />
+</template>
+
+<script>
+import VignetteArticle from "../components/VignetteArticle.vue"
+
+export default {
+   components: { VignetteArticle } 
+}
+</script>
