75f64cba30fbc7e93a4c6d2222bb9adc6b751c4a
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-17 16:29:39 +0200

    Header conseil 1 fini

--- src/views/Conseil1View.vue
+++ src/views/Conseil1View.vue
@@ -1,4 +1,17 @@
 <template>
+  <header class="bg-vert-pastel-deco">
+    <div class="flex justify-between pb-6 pt-1">
+      <div class="pt-8 pl-5">
+        <fleche-gauche/>
+      </div>
+      <div class="pt-3">
+        <LogoSite/>
+      </div>
+      <div class="pt-6 pr-5">
+        <IconConnexion/>
+      </div>
+    </div>
+  </header>
     <div>
         <VignetteArticle 
         image="/images/photo_dechet.webp"
@@ -10,8 +23,11 @@
 
 <script>
 import VignetteArticle from "../components/VignetteArticle.vue"
+import FlecheGauche from "../components/icons/FlecheGauche.vue"
+import LogoSite from "../components/icons/LogoSite.vue"
+import IconConnexion from "../components/icons/IconConnexion.vue"
 
 export default {
-   components: { VignetteArticle } 
+   components: { VignetteArticle, FlecheGauche, LogoSite, IconConnexion } 
 }
 </script>
