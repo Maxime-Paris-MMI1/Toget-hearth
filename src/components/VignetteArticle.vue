8e03774245c73056ef3e6df9ccab67ae80c8bec5
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 16:58:22 +0200

    changement des margins du composant vignette artcl

--- src/components/VignetteArticle.vue
+++ src/components/VignetteArticle.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="relative">
-        <img class="absolute w-[100%] max-h-[333px] min-h-[333px]" :src="image" alt="">
-        <div class="w-[100%] h-[107px] absolute mt-[266px] rounded-t-18xl  bg-blanc-background"></div>
-        <h1 class="absolute ml-5 mt-[150px] text-3xl font-asap text-left text-blanc-background">{{titre1}} <br> {{titre2}}</h1>
-        <h4 class="absolute ml-5 mr-6 mt-[300px] font-poppins text-left text-vert-kaki-elements-importants font-bold ">{{intro}}</h4>
+        <img class="w-[100%] max-h-[333px] min-h-[333px]" :src="image" alt="">
+        <div class="-mt-[290px]">
+          <div class="w-[100%] h-[107px] absolute mt-[266px] rounded-t-18xl  bg-blanc-background"></div>
+          <h1 class="absolute ml-5 mt-[150px] text-3xl font-asap text-left text-blanc-background">{{titre1}} <br> {{titre2}}</h1>
+          <h4 class="absolute ml-5 mr-6 mt-[300px] font-poppins text-left text-vert-kaki-elements-importants font-bold text-sm">{{intro}}</h4>
+        </div>
+        
     </div>
     
 </template>
--- src/views/Conseil1View.vue
+++ src/views/Conseil1View.vue
@@ -17,10 +17,11 @@
         image="/images/photo_dechet.webp"
         titre1="Éviter les"
         titre2="déchets plastiques"
-        intro="90% de la faune marine est impactée par la pollution du plastique ! "/>
-        <p>Face à cette situation, nous pouvons agir avec des petits gestes au quotidien. Lors de vos courses, optez pour des sacs en toile pour éviter l’utilisation de sachet plastique.</p>
-        
-        </div>
+        intro="90% de la faune marine est impactée par la pollution du plastique ! "/>  
+    </div>
+    <div>
+        <p class=" font-poppins text-xs ">Face à cette situation, nous pouvons agir avec des petits gestes au quotidien. Lors de vos courses, optez pour des sacs en toile pour éviter l’utilisation de sachet plastique.</p>
+    </div>
 </template>
 
 <script>
