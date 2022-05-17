857d3c1380dedfacde253dbd9d4b15918d8707ed
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 16:01:00 +0200

    Création de chemin d'accès

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <header>
-  
+   <nav>
+        <RouterLink to="/">Accueil</RouterLink>
+        <RouterLink to="/conseil1">Déchets Plastiques</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
 <script>
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
+import Conseil1 from '../views/Conseil1View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
-    // ici les autre routes
+    { path: '/conseil1', name: 'Conseil1', component: Conseil1 },
+
   ]
 })
 
