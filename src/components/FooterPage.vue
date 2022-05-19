9a8a3276eca0bbe236641b1e1c3cb2497c77a10f
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-19 15:14:30 +0200

    Avancement footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="absolute w-full z-0">
-        <VecteurFooter/>
-    </div>
-    <div class="flex-row justify-between z-10">
-        <FacebookLogo/>
-        <InstagramLogo/>
-        <TwitterLogo/>
+    <div class="mt-6">
+        <img class="w-screen max-h-20 -mb-1" src="../../public/images/Vecteur_Footer.webp" alt="">
+        <div class="flex justify-center gap-10 pt-1 pb-2 bg-vert-moyen-deco">
+            <FacebookLogo/>
+            <InstagramLogo/>
+            <TwitterLogo/>
+        </div>
+        <div class="bg-vert-moyen-deco flex-col justify-center">
+         <a href="">Défis</a>
+         <a href="">Accueil</a>
+         <a href="">À propos</a>
+         <a href="">Nous contacter</a>
+         <a href="">Mention légales/politique de confidentialité</a>
+        </div>
     </div>
 </template>
 
@@ -15,13 +22,10 @@
 import InstagramLogo from "../components/icons/InstagramLogo.vue" 
 import FacebookLogo from "../components/icons/FacebookLogo.vue" 
 import TwitterLogo from "../components/icons/TwitterLogo.vue" 
-import VecteurFooter from "../components/icons/VecteurFooter.vue" 
 
 export default {
   name: "Footer",
-  components: {InstagramLogo, FacebookLogo, TwitterLogo, VecteurFooter  },
-
-
+  components: {InstagramLogo, FacebookLogo, TwitterLogo  },
 };
 
 </script>
