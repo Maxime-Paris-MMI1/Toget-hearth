866ad55957cb2dfc36b0b903aa0bc52b1048105a
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-19 15:59:58 +0200

    ajout du lien back sur les flèches

--- src/components/HeaderFonce.vue
+++ src/components/HeaderFonce.vue
@@ -2,7 +2,7 @@
     <header class="bg-vert-pastel-deco">
     <div class="flex justify-between pb-6 pt-1">
       <div class="pt-8 pl-5">
-        <FlecheGauche/>
+        <a href="javascript:history.back()"><FlecheGauche/></a>
       </div>
       <div class="pt-3">
         <LogoSite/>
