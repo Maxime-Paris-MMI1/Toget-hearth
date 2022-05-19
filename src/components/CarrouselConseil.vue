227b33e89fbf5247c7df2edcd091db4cedba0f6d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-19 16:54:43 +0200

    Création du composants de la page conseils

---
+++ src/components/CarrouselConseil.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="m-5 w-[313px] h-[370px] relative">
+        <img class="rounded-13xl absolute w-full h-[313px] object-cover" :src="image" alt="">
+        <div>
+            <a href="">
+              <RouterLink to="/conseil1">
+                <button class="w-[158px] h-11 absolute left-[139.5px] top-[290.5px] rounded-18xl bg-vert-fluo-bouton">
+                  <p class="absolute text-lg ml-8 -mt-3 font-medium text-blanc-background">Lire la suite</p>
+                </button>
+              </RouterLink>
+            </a>
+        </div>
+        <h4 class="absolute text-xl leading-tight text-left ml-3 text-blanc-background mt-44">{{titre1}}<br />{{titre2}}</h4>
+        <p class="absolute text-xs text-left ml-3 mt-60 mr-14 text-blanc-background ">{{intro}}</p>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    image: String,
+    titre1: String,
+    titre2: String,
+    intro: String,
+  }
+}
+</script>
--- src/views/ConseilEcologique.vue
+++ src/views/ConseilEcologique.vue
@@ -3,7 +3,12 @@
     <HeaderClair/>
   </header>
   <main>
-
+    <h1 class="mt-10 ml-5 font-poppins text-2xl text-vert-fonce-typographie ">Conseils et Astuces écologiques</h1>
+    <CarrouselConseil 
+    image="/images/carroussel1.webp"
+    titre1="Éviter les"
+    titre2="déchets plastiques"
+    intro="90% de la faune marine est impactée par la pollution du plastique ! "/>
   </main>
   <footer>
     <FooterPage/>
@@ -12,10 +17,11 @@
 
 <script>
 import HeaderClair from "../components/HeaderClair.vue" 
+import CarrouselConseil from "../components/CarrouselConseil.vue"
 import FooterPage from "../components/FooterPage.vue" 
 
 export default {
   name: "Header",
-  components: {HeaderClair, FooterPage }
+  components: {HeaderClair, FooterPage, CarrouselConseil }
 };
 </script>
