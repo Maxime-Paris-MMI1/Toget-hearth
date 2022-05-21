f4bfdbf0d445d6cbf2132f716524d7e602162a92
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-21 19:51:48 +0200

    modification du bouton d'action

--- src/components/BoutonAction.vue
+++ src/components/BoutonAction.vue
@@ -3,8 +3,13 @@
   class="flex justify-center items-center relative gap-[6.723404407501221px] px-[29.000001907348633px] py-[7px] rounded-[56px] bg-[#45d475] ml-[88px] mr-[88px]"
   style="box-shadow: 0px 1.5600000619888306px 1.5600000619888306px 2.017021417617798px rgba(0,0,0,0.25);"
 >
-  <p class="flex-grow-0 flex-shrink-0 text-[16.136171340942383px] font-medium text-left text-white">
-    Envoyer
-  </p>
+  <p class="flex-grow-0 flex-shrink-0 text-[16.136171340942383px] font-medium text-left text-white">{{titrebouton}}</p>
 </div>
 </template>
+<script>
+export default {
+  props: {
+    titrebouton: String,
+  }
+}
+</script>
