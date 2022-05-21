4a5a1cd23952f861c6552a68a7b61560044b1770
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-21 18:06:50 +0200

    Finition de la page Conseil Ramassage

--- src/views/ConseilRamassage.vue
+++ src/views/ConseilRamassage.vue
@@ -13,19 +13,67 @@
             <img src="../../public\images\conseil_img1.webp" alt="Poubelle">
         </div>
         <div>
-            <p>
-                En effet lors d’un rammassage vous allez être confronter a plusieurs type de dechets
+            <p class="font-poppins text-xs font-medium ml-5 mr-14 mt-7">
+                En effet lors d’un ramassage vous allez être confronter a plusieurs type de dechets
                 dans la nature; verre, carton, mégots... Il faudra donc trier ces déchets pour faciliter
                 le travail dans les endroits de recyclage 
             </p>
         </div>
+        <div>
+            <h3 class="font-poppins leading-6 font-semibold mt-7 ml-28">
+                Respectez la nature !!
+            </h3>
+            <div class="flex justify-center">
+                <img class="mt-3" src="../../public\images\conseil_img2.webp" alt="Promeneur">
+            </div>
+            <p class="font-poppins text-xs font-medium ml-5 mt-4 mr-4">
+                Ne dégradez pas la nature! Si vous participez à un rammassage collectif,
+                ce n’est pas pour dégrader mais pour aider la nature.
+                Alors soyez indulgent et ne rammassez pas n’importe quel fleur,
+                ne déranger pas les animaux... En adoptant ces gestes simples vous aidez et respectez la planète
+                :)
+            </p>
+        </div>
+        <div>
+            <h3 class="font-poppins leading-6 font-semibold mt-7 ml-28">
+                Portez des protections 
+            </h3>
+            <div class="flex justify-center">
+                <img class="mt-3 " src="../../public\images\conseil_img3.webp" alt="Protections">
+            </div>
+            
+            <p class="font-poppins text-xs font-medium ml-5 mt-4 mr-4">
+                En effet dans la nature il exise des risques de blessures ou de maladies nottament
+                avec des bouts de verres coupants ou encore des seringues non stériles. Pour éviter
+                tout risques, portez des vêtements qui n’ont peu d’importance mais aussi des gants
+                de protections ainsi que des bonnes chaussures.
+            </p>
+        </div>
+        <div>
+            <h3 class="font-poppins leading-6 font-semibold mt-7 ml-28">
+                Ne prenez aucun risque 
+            </h3>
+            <div class="flex justify-center">
+                <img class="mt-3" src="../../public\images\conseil_img4.webp" alt="Croix">
+            </div>
+            
+            <p class="font-poppins text-xs font-medium ml-5 mt-7 mr-4 mb-14">
+                Si vous apercevez un déchet qui est au bord d’un précipice ou au beau millieu des ronces
+                n’y allez pas! Ces situations sont dangereuses et ne mérites pas d’être risqué.
+                il vaut mieux ne pas finir à l’hopital.
+            </p>
+        </div>
     </main>
+    <footer>
+        <FooterPage/>
+    </footer>
 </template>
 
 <script>
 import HeaderClair from "../components/HeaderClair.vue"
+import FooterPage from "../components/FooterPage.vue"
 
 export default {
-   components: { HeaderClair } 
+   components: { HeaderClair, FooterPage } 
 }
 </script>
