1b05ae19f062205bdb1596210e7ee4d20d4374e0
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-22 12:57:00 +0200

    Création du composant des vignettes news

---
+++ src/components/VignetteNews.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img class="mt-1 mr-5 rounded-5xl object-cover" :src="image" alt="">
+        <div>
+            <h4 class="font-asap ml-1 mt-4 text-xs font-medium text-vert-fonce-typographie ">{{title}}</h4>
+            <p class="font-asap ml-1 text-xs text-vert-fonce-typographie ">{{intro}}</p>
+            <RouterLink :to="lien">
+                  <p class="font-asap mt-1 ml-40 text-xs font-medium text-vert-fluo-bouton ">Lire plus</p>
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+export default {
+  props: {
+    image: String,
+    title: String,
+    intro: String,
+    lien:{
+      type: String,
+      default:"/"
+    },
+  }
+}
+</script>
