34aa1fe2cd85b2e25ba2efe2a58f2abf643e43c8
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-22 13:30:10 +0200

    Modif composant ajout underline + taille

--- src/components/CarrouselNews.vue
+++ src/components/CarrouselNews.vue
@@ -2,10 +2,10 @@
     <div class="w-[314px] h-[149.97px] relative">
         <img class="rounded-4xl absolute" :src="image" alt="">
         <div>
-            <h4 class="font-asap absolute mt-[72px] text-lg font-medium text-blanc-background ">{{title}}</h4>
-            <p class="font-asap absolute mt-24 text-xs font-medium text-blanc-background ">{{intro}}</p>
+            <h4 class="font-asap absolute ml-3 mt-[72px] text-lg font-medium text-blanc-background ">{{title}}</h4>
+            <p class="font-poppins absolute ml-3 mt-24 text-xs font-medium text-blanc-background ">{{intro}}</p>
             <RouterLink :to="lien">
-                  <p class="absolute text-base mr-3 mt-28 font-medium text-blanc-background">Lire</p>
+                  <p class="absolute font-poppins text-xs ml-[275px] mt-[122px] font-medium text-blanc-background underline">Lire</p>
               </RouterLink>
         </div>
     </div>
