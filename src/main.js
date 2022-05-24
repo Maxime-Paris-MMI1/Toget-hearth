358042b9fe297b044b7477b15f1d6041bc0481cd
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-24 16:07:23 +0200

    firebase test

--- src/main.js
+++ src/main.js
@@ -9,17 +9,15 @@ import './index.css'
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBkcGbdmUXu0VDj-LHK08E2Zz-GIa_C7Yw",
-    authDomain: "toget-hearth.firebaseapp.com",
-    projectId: "toget-hearth",
-    storageBucket: "toget-hearth.appspot.com",
-    messagingSenderId: "306387481357",
-    appId: "1:306387481357:web:0426fe586448a6ed057644"
-  };
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBkcGbdmUXu0VDj-LHK08E2Zz-GIa_C7Yw",
+  authDomain: "toget-hearth.firebaseapp.com",
+  projectId: "toget-hearth",
+  storageBucket: "toget-hearth.appspot.com",
+  messagingSenderId: "306387481357",
+  appId: "1:306387481357:web:0426fe586448a6ed057644"
+};
 // Initialize Firebase
 const appFirebase = initializeApp(firebaseConfig);
 
