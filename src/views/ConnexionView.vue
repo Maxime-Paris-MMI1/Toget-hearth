4b278a61ff7ac488b3a3d7e3ddd42e7abc8d8cbe
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-24 14:57:00 +0200

    Ajout du bouton connexion

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -6,13 +6,17 @@
     </RouterLink>
     </div>
     <h1 class="text-center font-asap text-xl font-normal">Connexion</h1>
+    <Bouton 
+    titrebouton="Se connecter"
+    lien="/"/>
 </template>
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import DecoHG from '../components/icons/DecoHG.vue'
 import LogoSite from '../components/icons/LogoSite.vue'
+import Bouton from '../components/BoutonAction.vue'
 
 export default {
-    components: { DecoHG, LogoSite },
+    components: { DecoHG, LogoSite, Bouton },
 }
 </script>
