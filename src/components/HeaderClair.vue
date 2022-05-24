85b134fa20ea9a9b2a661059a45e2a4ac379cd6a
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-24 21:15:37 +0200

    Début du menu

--- src/components/HeaderClair.vue
+++ src/components/HeaderClair.vue
@@ -9,8 +9,22 @@
       <div class="pt-5">
         <a href="/"><LogoSite/></a>
       </div>
+
+
+
       <div class="pt-12 pr-7">
-        <MenuHamburger/>
+        <button class="relative z-50 text-xl" aria-haspopup="true" aria-controls="menu" :aria-expanded="menuOuvert" @click="menuOuvert = !menuOuvert">
+          <MenuHamburger/>
+          <span class="sr-only">Menu</span>
+        </button>
+      </div>
+      <div id="menu" class="z-50 fixed inset-0 translate-x-full bg-red-200 motion-safe:duration-1000 motion-safe:transition-transform" v-if="menuOuvert"
+      :class="{ 'translate-x-0': menuOuvert }">
+        <ul>
+          <li><router-link to="/">Un</router-link></li>
+          <li><router-link to="/">Deux</router-link></li>
+          <li><router-link to="/">Trois</router-link></li>
+        </ul>
       </div>
     </div>
   </header>
@@ -23,6 +37,14 @@ import MenuHamburger from "../components/icons/MenuHamburger.vue"
 import LogoSite from "../components/icons/LogoSite.vue" 
 export default {
   name: "Header",
-  components: {IconConnexion, MenuHamburger, LogoSite}
+  components: {IconConnexion, MenuHamburger, LogoSite},
+  data() {
+    return {
+      menuOuvert: false,
+    };
+  },
+  beforeMount(){
+    this.$router.afterEach(() => (this.menuOuvert = false));
+  }
 };
 </script>
