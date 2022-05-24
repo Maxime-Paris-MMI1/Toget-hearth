f21d9b33d7037fb2c0fbf060841440a5058a3ac5
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-24 14:48:22 +0200

    Modif police du composants bouton

--- src/components/BoutonAction.vue
+++ src/components/BoutonAction.vue
@@ -4,7 +4,7 @@
   class="flex justify-center items-center relative gap-2 px-7 py-2 rounded-22xl bg-vert-fluo-bouton mx-[88px]"
   style="box-shadow: 0px 1.5600000619888306px 1.5600000619888306px 2.017021417617798px rgba(0,0,0,0.25);"
 >
-  <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-blanc-background drop-shadow-sm">{{titrebouton}}</p>
+  <p class="flex-grow-0 flex-shrink-0 text-base font-poppins font-medium text-left text-blanc-background drop-shadow-sm">{{titrebouton}}</p>
 </div>
 </RouterLink>
 </template>
