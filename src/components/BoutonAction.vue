b97e2717fb44f3fcfca7d41840fd24323fdfe33d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-24 14:27:42 +0200

    Modif des propriétés du composant bouton

--- src/components/BoutonAction.vue
+++ src/components/BoutonAction.vue
@@ -1,15 +1,19 @@
 <template>
+<RouterLink :to="lien">
 <div
-  class="flex justify-center items-center relative gap-[6.723404407501221px] px-[29.000001907348633px] py-[7px] rounded-[56px] bg-[#45d475] ml-[88px] mr-[88px]"
+  class="flex justify-center items-center relative gap-2 px-7 py-2 rounded-22xl bg-vert-fluo-bouton mx-[88px]"
   style="box-shadow: 0px 1.5600000619888306px 1.5600000619888306px 2.017021417617798px rgba(0,0,0,0.25);"
 >
-  <p class="flex-grow-0 flex-shrink-0 text-[16.136171340942383px] font-medium text-left text-white">{{titrebouton}}</p>
+  <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-blanc-background drop-shadow-sm">{{titrebouton}}</p>
 </div>
+</RouterLink>
 </template>
 <script>
+import { RouterLink, RouterView } from "vue-router";
 export default {
   props: {
     titrebouton: String,
+    lien: String,
   }
 }
 </script>
