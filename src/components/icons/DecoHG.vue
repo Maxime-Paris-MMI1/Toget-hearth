b8e2c953358897530df5ed64c2d463a44705387d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-24 14:22:29 +0200

    Ajout du contenus de la page connexion

---
+++ src/components/icons/DecoHG.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+  width="178"
+  height="203"
+  viewBox="0 0 178 203"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[199px] h-[218px]"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M92.9437 90.9504C151.504 64.8581 174.048 13.445 178 -9H-5V203C3.24789 176.522 34.3837 117.043 92.9437 90.9504Z"
+    fill="#C4DCBE"
+  ></path>
+  <path
+    d="M76.9437 84.9504C135.504 58.8581 158.048 7.445 162 -15H-21V197C-12.7521 170.522 18.3837 111.043 76.9437 84.9504Z"
+    fill="#84B777"
+  ></path>
+    </svg>
+</template>
---
+++ src/views/ConnexionView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-row justify-between">
+    <DecoHG />
+    <RouterLink to="/" >
+    <LogoSite class="mt-4 mr-5" />
+    </RouterLink>
+    </div>
+    <h1 class="text-center font-asap text-xl font-normal">Connexion</h1>
+</template>
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import DecoHG from '../components/icons/DecoHG.vue'
+import LogoSite from '../components/icons/LogoSite.vue'
+
+export default {
+    components: { DecoHG, LogoSite },
+}
+</script>
