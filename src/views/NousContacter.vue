85772c822ba0caf8222e0b19d32d1cda69ee303c
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-24 17:15:44 +0200

    HTML pour le formulaire

--- src/views/NousContacter.vue
+++ src/views/NousContacter.vue
@@ -12,8 +12,29 @@
       </p>
 
 
+          <div class="formulaire">
+           
+            <form action="/action_page.php">
+              <label for="fname">Nom </label>
+              <input type="text" id="fname" name="firstname" placeholder="Votre nom">
+          
+              <label for="sujet">Prénom</label>
+              <input type="text" id="sujet" name="sujet" placeholder="Votre prénom">
+          
+              <label for="emailAddress">Email</label>
+              <input id="emailAddress" type="email" name="email" placeholder="Votre email" required>
+          
+          
+              <label for="subject">Message</label>
+              <textarea id="subject" name="subject" placeholder="Votre message" style="height:200px"></textarea>
 
-      
+              <input class="condutilisation" type="checkbox" required> J’accepte les conditions d’utilisations 
+              <div class="sendbutton">
+              <input type="submit" value="Envoyer">
+              </div>
+            </form>
+          </div>
+
 
       <footer>
     <FooterPage/>
