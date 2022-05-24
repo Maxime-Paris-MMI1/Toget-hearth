b13d47a2c32d6deb01ac62103ee9a0f884f80067
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-24 14:42:06 +0200

    Problème résolu

--- src/components/BoutonAction.vue
+++ src/components/BoutonAction.vue
@@ -1,19 +1,22 @@
 <template>
-<!-- <RouterLink :to="lien"> -->
+<RouterLink :to="lien">
 <div
   class="flex justify-center items-center relative gap-2 px-7 py-2 rounded-22xl bg-vert-fluo-bouton mx-[88px]"
   style="box-shadow: 0px 1.5600000619888306px 1.5600000619888306px 2.017021417617798px rgba(0,0,0,0.25);"
 >
   <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-blanc-background drop-shadow-sm">{{titrebouton}}</p>
 </div>
-<!-- </RouterLink> -->
+</RouterLink>
 </template>
 <script>
 import { RouterLink, RouterView } from "vue-router";
 export default {
   props: {
     titrebouton: String,
-    // lien: String,
+    lien:{
+      type: String,
+      default:"/"
+    },
   }
 }
 </script>
