192b4a40ef9b9d9129e46bb43cff5050664cbbb9
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-27 11:38:21 +0200

    ajout des liens vers les réseaux sociaux du footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,9 +2,9 @@
     <div class="mt-6">
         <img class="w-screen max-h-20 -mb-1" src="../../public/images/Vecteur_Footer.webp" alt="">
         <div class="flex justify-center gap-10 pt-1 pb-2 bg-vert-moyen-deco">
-            <FacebookLogo/>
-            <InstagramLogo/>
-            <TwitterLogo/>
+            <a href="https://www.facebook.com/togetheartoff"><FacebookLogo/></a>
+            <a href="https://www.instagram.com/toget.heart_/"><InstagramLogo/></a>
+            <a href="https://twitter.com/heart_toget"><TwitterLogo/></a>
         </div>
         <div class=
         "flex
