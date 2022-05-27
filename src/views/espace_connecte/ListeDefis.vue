5fa27ccac8298e85f904d89c77c9cdf8b1a1bb5b
Bryan Ferry <bryan.ferry@edu.univ-fcomte.fr>
2022-05-27 13:50:44 +0200

    creation page liste défis finis

--- src/views/espace_connecte/ListeDefis.vue
+++ src/views/espace_connecte/ListeDefis.vue
@@ -4,7 +4,49 @@
     <HeaderClair/>
   </header>
 
+<main>
 
+  <h2 class=" text-center font-poppins text-2xl mt-16 mb-10">Liste des défis</h2>
+
+
+  <div class="space-x-1 mb-16">
+
+
+          <BoutonAction
+        :kaki="true"
+        titrebouton="Ramasser des déchets"
+        />
+
+        <BoutonAction
+        :kaki="true"
+        titrebouton="Venir en vélo au travail"
+        />
+
+        <BoutonAction
+        :kaki="true"
+        titrebouton="Organiser une collecte"
+        />
+
+        <BoutonAction
+        :kaki="true"
+        titrebouton="Se faire un repas 100% bio"
+        />
+
+        <BoutonAction
+        :kaki="true"
+        titrebouton="Supprimer des mails inutiles"
+        />
+
+        <BoutonAction
+        :kaki="true"
+        titrebouton="Sécher ses vêtements à l’air"
+        />
+
+
+  </div>
+
+        
+</main>
   <footer>
     <FooterPage/>
   </footer>
@@ -16,9 +58,10 @@ import { RouterLink, RouterView } from "vue-router";
 import HeaderClair from "../../components/HeaderClair.vue" 
 
 import FooterPage from "../../components/FooterPage.vue" 
+import BoutonAction from "../../components/Boutonaction.vue" 
 
 export default {
   name: "Header",
-  components: {HeaderClair, FooterPage }
+  components: {HeaderClair, FooterPage, BoutonAction, }
 };
 </script>
