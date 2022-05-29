addf26a552e648fb2ae66a181e73f9b0ac03ef57
Bryan Ferry <bryan.ferry@edu.univ-fcomte.fr>
2022-05-29 11:45:05 +0200

    fin de la page a propos

--- src/components/AProposCard.vue
+++ src/components/AProposCard.vue
@@ -1,34 +1,82 @@
 <template>
+<div class=" min-w-[360px] max-w-[445px] mx-auto my-auto w-full">
 
-<div class=" mt-10  "
- style="filter: drop-shadow(0px 8px 10px rgba(0,0,0,0.25));">
+<div class=" mt-3"
+ style="filter: drop-shadow(0px 8px 10px rgba(0,0,0,0.5));">
 
 
-<div class=" flex justify-center text-center">
+<div class=" flex justify-center text-center ">
 
-<div class="  absolute">
+<div class="  absolute max-w-xl mt-10">
 
     <div class=" flex justify-center">
-<img class=" h-2/5 px-10 pt-10 " src="../../public/images/maxime.webp" alt=" maxime paris">
+<img class=" h-2/5 px-10 pt-10  " :src="image" alt="">
 
 </div>
 
-<h2 class=" font-asap text-3xl  ">Maxime Paris</h2>
-<h3 class="font-poppins text-xl">BUT MMI - Première année </h3>
-
-
-
-<p class="font-poppins text-sm p-20 ">Bonjour! je m’appelle Maxime Paris et pour ce projet, j’ai pu réaliser le logo ainsi qu’une partie de la maquette graphique. Je compte m’orienter vers le design plus tard pour faire infographiste. </p>
-
+<h2 class=" font-asap text-2xl  ">{{nom}}</h2>
+<h3 class="font-poppins text-lg   ">{{formation}} </h3>
 
 
 </div>
 </div>
 
+    <img class=" object-cover mx-auto my-auto w-full pl-10 pr-10 pt-16 max-w-[700px] " src="../../public/images/rectangle1_apropos.jpg" alt="fond a propos">
 
-    <img class=" object-cover mx-auto my-auto w-full p-10" src="../../public/images/fond_apropos.jpg" alt="fond a propos">
+</div>
+
+<div>
+
+<p class="font-poppins text-sm p-20 absolute z-20 mt-20 break-words w-[400px] overflow-hidden pl-22 ">{{texte_description}}</p>
+
+
+    <img class=" object-cover mx-auto my-auto w-full p-10 max-w-[700px] -mt-40  " src="../../public/images/rectangle2_apropos.webp" alt="fond a propos"
+    style="filter: drop-shadow(0px 8px 10px rgba(0,0,0,0.5));">
 
     
 </div>
+</div>
+
+<div class=" flex justify-center space-x-10 w-full">
+
+<a :href="lienlinkedin">  <img src="../../public/images/linkedin.webp" alt="linkedin"> </a>
+<a :href="lieninsta"> <img src="../../public/images/instagram.webp" alt="instagram"> </a> 
+
+</div>
+
+
 
 </template>
+
+<script>
+
+export default {
+    props: {
+         image: {type: String,
+       
+        },
+        lienlinkedin: {type: String,
+       
+        },
+        lieninstagram: {type: String,
+       
+        },
+        
+
+        nom: {type: String,
+        default:"Jean Dupont"
+        },
+
+        formation: {type: String,
+        default:"BUT MMI"
+        },
+        
+
+        texte_description:{ type: String,
+        default:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada urna vitae quam ultrices, sit amet facilisis nibh dapibus. Sed id felis vel quam sollicitudin convallis et quis sapien. Vestibulum leo nunc, dignissim in malesuada ac, efficitur sed tortor. Duis non nulla eu nunc commodo tincidunt vitae molestie libero. Etiam lacinia bibendum lectus. Ut non arcu gravida, consequat urna et, pretium ex."},
+
+    
+    },
+ 
+}
+</script>
--- src/views/APropos.vue
+++ src/views/APropos.vue
@@ -12,7 +12,33 @@
 <h2 class=" font-asap font-medium text-xl text-center mt-10 mb-10 ">Bienvenue sur la page à propos ! </h2>
 <p class=" font-poppins font-medium text-base mt-8mb-10 pl-10 pr-10 ">Ici vous trouverez les personnes qui ont pû réaliser ce site web dans le cadre de notre projet collectif au sein du département MMI de montbéliard </p>
 
-<AProposCard/>
+<AProposCard
+   image="../../public/images/maxime.webp"
+
+    lienlinkedin="https://www.linkedin.com/in/maxime-paris-730322215/"
+    lieninsta="https://www.linkedin.com/in/maxime-paris-730322215/"
+
+    nom="Maxime Paris"
+    formation="BUT MMI - Première année "
+    texte_description="Bonjour! je m’appelle Maxime Paris et pour ce projet, j’ai pu réaliser le logo ainsi qu’une partie de la maquette graphique. Je compte m’orienter vers le design plus tard pour faire infographiste. "/>
+<AProposCard
+  image="../../public/images/bryan.webp"
+
+      lienlinkedin="https://www.linkedin.com/in/bryan-ferry-523546216/"
+    lieninsta="https://www.instagram.com/bryan.fzz/"
+
+    nom="Bryan Ferry"
+    formation="BUT MMI - Première année "
+    texte_description="Bonjour je m’appelle Bryan ferry et pour ce projet j’ai réaliser une partie de la maquette ainsi que le guide de style, j’ai également développer la landing page. Je compte m’orienter vers le design plus tard pour devenir UX designer.  "/>
+<AProposCard
+  image="../../public/images/justine.webp"
+
+    lienlinkedin="https://www.linkedin.com/in/justine-duval-28b3a2224/"
+    lieninsta="https://www.instagram.com/jdxl.pro/?hl=fr"
+
+    nom="Justine Duval"
+    formation="BUT MMI - Première année "
+    texte_description="Bonjour, je m’appelle Justine Duval ! Pour ce projet je me suis occupée de la partie communication et du design avec la réalisation de certaines page de cette maquette graphique. Plus tard j’aimerai devenir Infographiste ou Web-Designer. "/>
 
 
 
@@ -31,7 +57,8 @@ import HeaderClair from "../components/HeaderClair.vue"
 import FooterPage from "../components/FooterPage.vue" 
 import AProposCard from "../components/AProposCard.vue" 
 
+
 export default {
-  components: {HeaderClair, FooterPage, AProposCard, }
+  components: {HeaderClair, FooterPage, AProposCard,  }
 };
 </script>
