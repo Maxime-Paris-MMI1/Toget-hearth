37f1ae9759b181e234da3f83b940ca7c6699408a
Bryan Ferry <bryan.ferry@edu.univ-fcomte.fr>
2022-06-08 18:55:57 +0200

    fin de la page precaution

--- src/views/espace_connecte/Precaution.vue
+++ src/views/espace_connecte/Precaution.vue
@@ -14,7 +14,7 @@
             <p class="  font-poppins  font-semibold ml-5">
                 Triez vos déchets dans plusieurs bacs différents pour faciliter le travail
             </p>
-<img class=" w-20 h-20" src="/public/images/Validation.svg" alt=" validation checkbox">             
+<img class=" w-16 h-16" src="/public/images/Validation.svg" alt=" validation checkbox">             
         </div>
 
 
@@ -33,12 +33,12 @@
             <p class="  font-poppins  font-semibold ml-5">
                 Respectez la nature !!
             </p>
-<img class=" w-20 h-20" src="/public/images/Validation.svg" alt=" validation checkbox">             
+<img class=" w-16 h-16" src="/public/images/Validation.svg" alt=" validation checkbox">             
         </div>
 
 
         <div class=" flex justify-center ">
-<img class=" absolute mt-44 ml-10 w-52 z-10 " src="/public\images\conseil_img2.webp" alt="Nature">
+<img class=" absolute mt-44  w-60 z-10 " src="/public\images\conseil_img2.webp" alt="Nature">
 </div>
 <img class=" mt-8 object-cover w-screen max-w-xl mr-auto mb-16 rotate-180" src="/public/images/fond_precaution1.webp" alt=" image precaution">
 
@@ -48,55 +48,71 @@
 <!-- partie -->
 <div>
 
-        <div class=" flex absolute mt-[50px] leading-6 p-10 z-10 items-center gap-3 ">
+        <div class=" flex absolute mt-[15px] leading-6 p-10 z-10 items-center gap-3 ">
             <p class="  font-poppins  font-semibold ml-5">
                Portez des protections 
             </p>
-<img class=" w-20 h-20" src="/public/images/Validation.svg" alt=" validation checkbox">             
+<img class=" w-16 h-16" src="/public/images/Validation.svg" alt=" validation checkbox">             
         </div>
 
 
         <div class=" flex justify-center ">
-<img class=" absolute mt-44 ml-10 w-52 z-10 " src="/public\images\conseil_img2.webp" alt="Nature">
+<img class=" absolute mt-28 ml-7 w-52 z-10 " src="/public\images\conseil_img3.webp" alt="accessoire de protection">
 </div>
 <img class=" mt-8 object-cover w-screen max-w-xl mr-auto mb-16 " src="/public/images/fond_precaution2.webp" alt=" image precaution">
 
 </div>
 
+<!-- partie -->
+<div>
 
-
-
-        <div>
-            <h3 class="font-poppins leading-6 font-semibold mt-7 ml-28">
-                Portez des protections 
-            </h3>
-            <div class="flex justify-center">
-                <img class="mt-3 " src="/public\images\conseil_img3.webp" alt="Protections">
-            </div>
-            
-            <p class="font-poppins text-xs font-medium ml-5 mt-4 mr-4">
-                En effet dans la nature il exise des risques de blessures ou de maladies nottament
-                avec des bouts de verres coupants ou encore des seringues non stériles. Pour éviter
-                tout risques, portez des vêtements qui n’ont peu d’importance mais aussi des gants
-                de protections ainsi que des bonnes chaussures.
+        <div class=" flex absolute mt-[15px] leading-6 p-10 z-10 items-center gap-3 ">
+            <p class="  font-poppins  font-semibold ml-5">
+               Ne prenez aucun risque  
             </p>
+<img class=" w-16 h-16" src="/public/images/Validation.svg" alt=" validation checkbox">             
         </div>
-        <div>
-            <h3 class="font-poppins leading-6 font-semibold mt-7 ml-28">
-                Ne prenez aucun risque 
-            </h3>
-            <div class="flex justify-center">
-                <img class="mt-3" src="/public\images\conseil_img4.webp" alt="Croix">
-            </div>
-            
-            <p class="font-poppins text-xs font-medium ml-5 mt-7 mr-4 mb-14">
-                Si vous apercevez un déchet qui est au bord d’un précipice ou au beau millieu des ronces
-                n’y allez pas! Ces situations sont dangereuses et ne mérites pas d’être risqué.
-                il vaut mieux ne pas finir à l’hopital.
-            </p>
-        </div>
+
+
+        <div class=" flex justify-center ">
+<img class=" absolute mt-32  w-16 z-10 " src="/public\images\conseil_img4.webp" alt="croix">
+</div>
+<img class=" mt-8 object-cover w-screen max-w-xl mr-auto mb-16 rotate-180 " src="/public/images/fond_precaution2.webp" alt=" image precaution">
+
+</div>
+
+<p class=" font-poppins font-light p-10 mb-7">
+    Pour plus d’informations vous pouvez lire notre page spécialement conçu pour les précautions à adopter avant et pendant un ramassage. 
+</p>
+
+              <BoutonAction 
+                :fluo="true"
+                titrebouton= "En savoir plus "/>
+
+
+<div class=" flex justify-center items-center p-7 mt-6 mb-16">
+
+    <p class="font-poppins font-light text-xs ">
+        J’ai pris connaissance des conditions avant le ramassage et je m’engage à les respecter 
+    </p>
+<input type="checkbox" required >
+    
+</div>
+
+
+              <BoutonAction 
+                :fluo="true"
+                titrebouton= "Organiser un rendez-vous "
+                
+                />
+
+
+
+
+
+
     </main>
-    <footer>
+    <footer class=" mt-20">
         <FooterPage/>
     </footer>
 </template>
@@ -104,8 +120,9 @@
 <script>
 import HeaderClair from "../../components/HeaderClair.vue"
 import FooterPage from "../../components/FooterPage.vue"
+import BoutonAction from "../../components/BoutonAction.vue"
 
 export default {
-   components: { HeaderClair, FooterPage } 
+   components: { HeaderClair, FooterPage,BoutonAction } 
 }
 </script>
